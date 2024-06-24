80b0f2386d7ea19b5482d9b3e65f7cad56a01a0f
jmcknight17 <jmcknight17@qub.ac.uk>
2024-06-24 21:04:52 +0100

    Hero-Updates
    - Relinking CV
    - Adding light logos for dark mode
    - Rewrite of description.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,9 +3,12 @@ import themeIcon from "../assets/sun.svg";
 import heroImg from "../assets/hero-img.svg";
 import sun from "../assets/sun.svg";
 import moon from "../assets/moon.svg";
-import twitterIcon from "../assets/twitter-light.svg";
-import linkedInIcon from "../assets/linkedin-light.svg";
-import githubIcon from "../assets/github-light.svg";
+import twitterLight from "../assets/twitter-light.svg";
+import linkedInLight from "../assets/linkedin-light.svg";
+import githubLight from "../assets/github-light.svg";
+import twitterDark from "../assets/twitter-dark.svg";
+import githubDark from "../assets/github-dark.svg";
+import linkedInDark from "../assets/linkedin-dark.svg";
 import CV from "../assets/cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
@@ -13,6 +16,10 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === "light" ? sun : moon;
+
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const linkedInIcon = theme === "light" ? linkedInLight : linkedInDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -32,26 +39,24 @@ function Hero() {
         </h1>
         <h2>Junior Software Engineer</h2>
         <span>
-          <a href="https://x.com/jmck_j" targey="_blank">
+          <a href="https://x.com/jmck_j" target="_blank">
             <img src={twitterIcon} alt="Twitter Icon" />
           </a>
           <a
             href="https://www.linkedin.com/in/jay-mcknight-253311274/"
-            targey="_blank"
+            target="_blank"
           >
             <img src={linkedInIcon} alt="Twitter Icon" />
           </a>
-          <a
-            href="https://github.com/https://github.com/jmcknight17"
-            targey="_blank"
-          >
+          <a href="https://github.com/jmcknight17" target="_blank">
             <img src={githubIcon} alt="Twitter Icon" />
           </a>
         </span>
         <p className={styles.description}>
-          A young developer with a passion to grow personal and professional
-          development skills. Eager to learn and apply problem solving skills to
-          any projects that need it.
+          A junior developer with a passion for growing both personal and
+          professional development skills. Eager to apply my problem-solving
+          abilities, honed through two years of industry experience and a
+          Bachelor's degree in Computing.
         </p>
         <a href={CV} download>
           <button className="hover">Resume</button>
