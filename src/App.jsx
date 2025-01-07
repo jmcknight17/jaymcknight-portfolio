c8d3c5f2d84c7b32e3d76a87e462f06051c3a739
jaymcknight1 <jmcknight17@qub.ac.uk>
2025-01-07 11:50:53 +0000

    Base template for Timeline Component. Styling and content changes required

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Skills from "./Components/Skills";
 import Contact from "./Components/Contact";
 import "./App.css";
 import Footer from "./Components/Footer";
+import Timeline from "./Components/Timeline";
 
 function App() {
   return (
     <>
       <Hero />
+        <Timeline />
       <Projects />
       <Skills />
       <Contact />
